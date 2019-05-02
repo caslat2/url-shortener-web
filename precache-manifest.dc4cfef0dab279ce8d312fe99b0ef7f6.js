50a87f3da20ca8c5848f3be65fb7e1e149ee4410
yaoandy107 <yaoandy107@gmail.com>
2019-05-03 00:58:16 +0800

    deploy

---
+++ precache-manifest.dc4cfef0dab279ce8d312fe99b0ef7f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcc58a96d6a2de084b70",
+    "url": "/url-shortener-web/css/about.0efeb5fa.css"
+  },
+  {
+    "revision": "bcc58a96d6a2de084b70",
+    "url": "/url-shortener-web/js/about.b2e8b462.js"
+  },
+  {
+    "revision": "16e0226b4acf2d313567",
+    "url": "/url-shortener-web/css/app.d73c5cf4.css"
+  },
+  {
+    "revision": "16e0226b4acf2d313567",
+    "url": "/url-shortener-web/js/app.6b7e0364.js"
+  },
+  {
+    "revision": "ac718da56d84f71be586",
+    "url": "/url-shortener-web/css/chunk-vendors.af67b8ba.css"
+  },
+  {
+    "revision": "ac718da56d84f71be586",
+    "url": "/url-shortener-web/js/chunk-vendors.3da5a4c9.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/url-shortener-web/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/url-shortener-web/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3527447ab1da02e03ad463f3bef0c6f3",
+    "url": "/url-shortener-web/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/url-shortener-web/robots.txt"
+  }
+];
